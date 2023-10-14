95c99e04f24d7425d0535a6164ab0d6b68b3a893
young37 <younghk37@gmail.com>
2023-10-14 20:19:28 +0900

    fix: 경로 오타 수정

--- src/main/java/younghk37/shop/wanted/recruitment/presentation/controller/RecruitmentController.java
+++ src/main/java/younghk37/shop/wanted/recruitment/presentation/controller/RecruitmentController.java
@@ -14,13 +14,13 @@ public class RecruitmentController {
 
     private final RecruitmentService recruitmentService;
 
-    @PostMapping("recruitment-announcement")
+    @PostMapping("/recruitment-announcement")
     public ResponseEntity<?> createRecruitmentAnnouncement(@RequestBody RecruitmentAnnouncementCreationReqDto reqDto) {
         recruitmentService.createRecruitmentAnnouncement(reqDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
-    @PutMapping("recruitment-announcement/{id}")
+    @PutMapping("/recruitment-announcement/{id}")
     public ResponseEntity<?> createRecruitmentAnnouncement(@PathVariable Long id, @RequestBody RecruitmentAnnouncementModifyReqDto reqDto) {
         recruitmentService.modifyRecruitmentAnnouncement(id, reqDto);
         return ResponseEntity.ok(null);
